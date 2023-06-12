c730198e2b60b103de747ee4f9b2555f1827a20f
Filip Patyk <fp.patyk@gmail.com>
2023-06-13 01:15:35 +0200

    feat: add multitabs and static plots

--- dashboard/layout/callbacks/select_tabs.py
+++ dashboard/layout/callbacks/select_tabs.py
@@ -4,6 +4,13 @@ from dashboard.layout.tab_1 import tab_1
 from dashboard.layout.tab_2 import tab_2
 from dashboard.layout.tab_3 import tab_3
 from dashboard.layout.year_tab import year_tab
+
+# import tabs for year tab
+from dashboard.layout.year_map import year_map
+from dashboard.layout.year_stats import year_stats
+
+
+# from dashboard.layout.stats_year import stats_year
 from dash.dependencies import Input, Output
 
 
@@ -16,5 +23,15 @@ def select_tabs(tab):
         return tab_2
     elif tab == "tab-3":
         return tab_3
-    elif tab == "yaer-tab":
+    elif tab == "travel-year-tab":
         return year_tab
+    # elif tab == "stats-year-tab":
+    #     return stats_year
+
+
+@app.callback(Output("tabs-year-content", "children"), Input("year-tabs", "value"))
+def select_year_tabs(tab):
+    if tab == "year-map":
+        return year_map
+    elif tab == "year-stats":
+        return year_stats
--- dashboard/layout/callbacks/select_year.py
+++ dashboard/layout/callbacks/select_year.py
@@ -1,14 +1,20 @@
+import numpy as np
 from dashboard.index import app
-
-
+import pandas as pd
+import base64
+from io import BytesIO
 import plotly.graph_objs as go
 from dashboard.layout.year_tab import races_df, circuits_df
 from dash.dependencies import Input, Output
+import seaborn as sns
+import matplotlib.pyplot as plt
+import time
+import matplotlib
+from datetime import datetime
 
-
-# def update_graph(col_chosen):
-#     fig = px.histogram(df, x="continent", y=col_chosen, histfunc="avg")
-#     return fig
+matplotlib.use("agg")
+lap_times_df = pd.read_csv("data/lap_times.csv")
+drivers_df = pd.read_csv("data/drivers.csv")
 
 
 @app.callback(
@@ -99,3 +105,93 @@ def travel_map_by_year(year):
     )
 
     return fig
+
+
+@app.callback(
+    Output(component_id="race-fig-map", component_property="src"),
+    # Output(component_id="race-pace-fig", component_property="figure"),
+    Input(component_id="race-dropdown", component_property="value"),
+)
+def update_box_plot(raceId):
+    df_boxplot = lap_times_df.loc[lap_times_df["raceId"] == raceId]
+
+    df_boxplot = df_boxplot[["driverId", "time", "milliseconds"]]
+
+    df_boxplot = pd.merge(df_boxplot, drivers_df[["driverId", "surname"]], on="driverId")
+
+    driver_names = df_boxplot["surname"].unique().tolist()
+    boxplot_res = pd.DataFrame()
+    for driver in driver_names:
+        driver_data = df_boxplot.loc[df_boxplot["surname"] == driver].sort_values(by="milliseconds")
+        boxplot_res = pd.concat([boxplot_res, driver_data])
+
+    df_boxplot = df_boxplot
+
+    fig, ax = plt.subplots(figsize=(16, 9), dpi=120)
+    boxplot = sns.boxplot(data=boxplot_res, x="milliseconds", y="surname", orient="h")
+    formatter = matplotlib.ticker.FuncFormatter(lambda ms, _: time.strftime("%M:%S.000", time.gmtime(ms / 1000)))
+    ax.xaxis.set_major_formatter(formatter)
+
+    ax.grid(True)
+    ax.set_xlabel("Lap time")
+    ax.set_ylabel("Driver surname")
+    ax.set_title("Drivers race pace")
+
+    buf = BytesIO()
+    fig.savefig(buf, format="png")
+    plt.close()
+    data = base64.b64encode(buf.getbuffer()).decode("ascii")
+    buf.close()
+    return f"data:image/png;base64,{data}"
+
+
+@app.callback(
+    Output(component_id="race-pace-fig", component_property="figure"),
+    Input(component_id="race-dropdown", component_property="value"),
+)
+def plot_pace_fig(raceId):
+    df_line = lap_times_df.loc[lap_times_df["raceId"] == raceId]
+
+    data = [
+        go.Scatter(
+            x=df_line["lap"][df_line["driverId"] == driver],
+            y=pd.to_timedelta(df_line["milliseconds"][df_line["driverId"] == driver], unit="ms")
+            + pd.to_datetime("1970/01/01"),  # NEEDS TO ADD DATETIME FOR PLOTLY TO WORK
+            name=f"{drivers_df['forename'][drivers_df['driverId'] == driver].values[0]}\
+ {drivers_df['surname'][drivers_df['driverId'] == driver].values[0]}",
+        )
+        for driver in df_line["driverId"].unique()
+    ]
+
+    layout = go.Layout(
+        title="Race pace",
+        yaxis=dict(
+            title="time",
+            tickformat="%M:%S.000",
+        ),
+        xaxis=dict(title="lap"),
+    )
+
+    for i in range(len(data)):
+        data[i]["visible"] = True if i < 5 else "legendonly"
+        # data[i]["visible"] = "legendonly"
+    fig = go.Figure(data=data, layout=layout)
+
+    return fig
+
+
+@app.callback(
+    [
+        Output(component_id="race-dropdown", component_property="options"),
+        Output(component_id="race-dropdown", component_property="value"),
+    ],
+    [Input(component_id="years-dropdown", component_property="value")],
+)
+def update_races_year_dropdown(year):
+    races_in_year = races_df[["raceId", "name"]][races_df["year"] == year]
+    # # print(races_in_year, type(races_in_year))
+    # x = [i for i in races_in_year.to_dict("records")]
+    # # print(*x, sep="\n")
+    races = races_in_year.to_dict("records")
+    # print(races[0]["name"])
+    return [{"label": i["name"], "value": i["raceId"]} for i in races], races[0]["raceId"]
